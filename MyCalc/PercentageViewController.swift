c603ec3461d3f6086625cd8e92d3ab175f21568e
Kitamura Satoshi <with.no.parachute@gmail.com>
2018-08-19 18:35:49 +0900

    Implement percentage view controller and result view controller

---
+++ MyCalc/PercentageViewController.swift
@@ -0,0 +1,91 @@
+//
+//  PercentageViewController.swift
+//  MyCalc
+//
+//  Created by 北村聡士 on 2018/08/19.
+//  Copyright © 2018年 satooshi. All rights reserved.
+//
+
+import UIKit
+
+class PercentageViewController: UIViewController {
+    var price: UInt64 = 0
+
+    @IBOutlet weak var percentageField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tap9Button(_ sender: Any) {
+        tapNumberButton(number: "9")
+    }
+    
+    @IBAction func tap8Button(_ sender: Any) {
+        tapNumberButton(number: "8")
+    }
+    
+    @IBAction func tap7Button(_ sender: Any) {
+        tapNumberButton(number: "7")
+    }
+    
+    @IBAction func tap6Button(_ sender: Any) {
+        tapNumberButton(number: "6")
+    }
+    
+    @IBAction func tap5Button(_ sender: Any) {
+        tapNumberButton(number: "5")
+    }
+    
+    @IBAction func tap4Button(_ sender: Any) {
+        tapNumberButton(number: "4")
+    }
+    
+    @IBAction func tap3Button(_ sender: Any) {
+        tapNumberButton(number: "3")
+    }
+    
+    @IBAction func tap2Button(_ sender: Any) {
+        tapNumberButton(number: "2")
+    }
+    
+    @IBAction func tap1Button(_ sender: Any) {
+        tapNumberButton(number: "1")
+    }
+    
+    @IBAction func tap0Button(_ sender: Any) {
+        tapNumberButton(number: "0")
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+        percentageField.text = "0"
+    }
+
+    // privateにしたい
+
+    func tapNumberButton(number: String) {
+        let value = percentageField.text! + number
+        
+        if let price = UInt64(value) {
+            percentageField.text = "\(price)"
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyCalc/ResultViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ResultViewController.swift
+//  MyCalc
+//
+//  Created by 北村聡士 on 2018/08/19.
+//  Copyright © 2018年 satooshi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var price: UInt = 0
+    var discountRatio: UInt = 0
+
+    @IBOutlet weak var resultField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let percentage = Float(discountRatio) / 100
+        let discountAmount = Float(price) * percentage
+        let discountPrice = price - UInt(discountAmount)
+
+        resultField.text = "\(discountPrice)"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
