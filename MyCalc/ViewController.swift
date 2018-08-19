c30f8465d70b3e454bfb1e82ca27e02314c72674
Kitamura Satoshi <with.no.parachute@gmail.com>
2018-08-19 17:22:03 +0900

    Implement calc buttons, price field actions

--- MyCalc/ViewController.swift
+++ MyCalc/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var priceField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,63 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+ 
+    // actions
 
+    @IBAction func tap1Button(_ sender: Any) {
+        tapNumberButton(number: "1")
+    }
+    
+    @IBAction func tap2Button(_ sender: Any) {
+        tapNumberButton(number: "2")
+    }
+    
+    @IBAction func tap3Button(_ sender: Any) {
+        tapNumberButton(number: "3")
+    }
+    
+    @IBAction func tap4Button(_ sender: Any) {
+        tapNumberButton(number: "4")
+    }
+    
+    @IBAction func tap5Button(_ sender: Any) {
+        tapNumberButton(number: "5")
+    }
+    
+    @IBAction func tap6Button(_ sender: Any) {
+        tapNumberButton(number: "6")
+    }
+    
+    @IBAction func tap7Button(_ sender: Any) {
+        tapNumberButton(number: "7")
+    }
+    
+    @IBAction func tap8Button(_ sender: Any) {
+        tapNumberButton(number: "8")
+    }
+    
+    @IBAction func tap9Button(_ sender: Any) {
+        tapNumberButton(number: "9")
+    }
+    
+    @IBAction func tap0Button(_ sender: Any) {
+        tapNumberButton(number: "0")
+    }
+    
+    @IBAction func tap00Button(_ sender: Any) {
+        tapNumberButton(number: "00")
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+        priceField.text = "0"
+    }
+    
+    func tapNumberButton(number: String) {
+        let value = priceField.text! + number
+        
+        if let price = UInt64(value) {
+            priceField.text = "\(price)"
+        }
+    }
 }
 
